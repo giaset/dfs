bfee0d1209b9b948a977849a8213361284f90e93
Gianni Settino <g@miltonandparc.com>
2015-01-04 13:18:01 -0500

    Get players!

--- get-player-ids.js
+++ get-player-ids.js
@@ -7,6 +7,57 @@ var teams = fb.child('teams')
 var players = fb.child('players')
 
 teams.once('value', function(snapshot) {
-	console.log(snapshot.val())
-	process.exit()
+	var teamsObject = snapshot.val()
+	for (team in teamsObject) {
+		//console.log(team)
+
+		//http://www.nhl.com/ice/playersearch.htm?team=BUF#
+		var url = 'http://www.nhl.com/ice/playersearch.htm?team='+team+'#'
+		request(url, function(error, reponse, html) {
+			if (!error) {
+				var $ = cheerio.load(html)
+				$('table.data tr').each(function(index, row) {
+					var player = {}
+
+					$('td', this).each(function(index, cell) {
+						var cellValue = $(this).text()
+						switch(index) {
+							case 0:
+							var withoutNewLines = cellValue.substr(1, cellValue.length-2)
+							// MacKinnon, Nathan (C)
+							var commaSplit = withoutNewLines.split(',')
+							player.last_name = commaSplit[0]
+							//  Nathan (C) *note the space*
+							var secondPart = commaSplit[1].substr(1)
+							var parenthesesSplit = secondPart.split('(')
+							player.first_name = parenthesesSplit[0].substr(0, parenthesesSplit[0].length-1)
+							break
+
+							case 1:
+							player.team = cellValue.substr(0, cellValue.length-1)
+							break
+						}
+					})
+
+					if (!isEmpty(player)) {
+						console.log(player)
+					}
+				})
+			} else {
+				console.log(error)
+			}
+		})
+
+	}
+	//process.exit()
 })
+
+function isEmpty(obj) {
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop)) {
+			return false
+		}
+	}
+
+	return true
+}
